ea2f597d83520afbce8f456c58fd63c4614b03b1
raulfabra20 <raulfabragila@gmail.com>
2024-10-09 10:27:13 +0200

    V.1

---
+++ src/main/java/cat/itacademy/s04/t02/n01/repository/FruitRepository.java
@@ -0,0 +1,12 @@
+package cat.itacademy.s04.t02.n01.repository;
+
+import cat.itacademy.s04.t02.n01.model.Fruit;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface FruitRepository extends JpaRepository<Fruit, Integer> {
+
+}
